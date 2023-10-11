13548d0e3b7576027bc9cb2973705ba629aea029
maraalina <mara_ispas@yahoo.com>
2023-10-11 14:50:12 +0200

    Update 4 - CustomerServiceGrpc

--- src/main/java/server/CustomerServiceImpl.java
+++ src/main/java/server/CustomerServiceImpl.java
@@ -1,7 +1,31 @@
 package server;
 
+import customer.CreateCustomerRequest;
+import customer.CreateCustomerResponse;
 import customer.CustomerServiceGrpc;
+import dao.AccountDAO;
+import dao.CustomerDAOInterface;
+import io.grpc.stub.StreamObserver;
+
+import java.sql.SQLException;
 
 public class CustomerServiceImpl extends CustomerServiceGrpc.CustomerServiceImplBase
 {
+  private CustomerDAOInterface dao;
+
+
+  @Override public void createCustomer(CreateCustomerRequest request, StreamObserver<CreateCustomerResponse> responseObserver)
+  {
+    try
+    {
+      dao.createCustomer(request.getFirstName(), request.getLastName(), request.getPhoneNumber(), request.getEmail(), request.getPassword());
+      CreateCustomerResponse response = CreateCustomerResponse.newBuilder().build();
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    }
+    catch (SQLException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
 }
